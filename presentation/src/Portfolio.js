b8ba7b1213ebe98c84ef282515ae808dd0b7f516
Fatih Tunahan Dumanli <fatihdumanli@live.com>
2020-11-04 11:47:34 +0300

    project items formatted

--- presentation/src/Portfolio.js
+++ presentation/src/Portfolio.js
@@ -1,4 +1,5 @@
 import { Component, React } from 'react'
+import ProjectItem from './ProjectItem'
 
 class Portfolio extends Component {
     render() {
@@ -8,8 +9,14 @@ class Portfolio extends Component {
                     <h3 className="h3 sectionHeader"> {this.props.title}</h3>
 
                 </div>
-                <p className="overviewText">
-                </p>
+
+                <ProjectItem title="EventTower" description="Message bus" 
+                    link="https://github.com/fatihdumanli/EventTower"
+                    thumbnail="https://i.ibb.co/D71bFdC/event-tower-128x.png"></ProjectItem>
+                <ProjectItem title="NPrismy" description="Lightweight orm" 
+                    link="https://github.com/fatihdumanli/EventTower"
+                    thumbnail="https://raw.githubusercontent.com/fatihdumanli/NPrismy/master/nprismy-logo.png"></ProjectItem>
+
             </div>
         )
     }
---
+++ presentation/src/ProjectItem.js
@@ -0,0 +1,30 @@
+import { React, Component } from 'react'
+
+class ProjectItem extends Component {
+
+
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div className="row project-item">
+                <div className="col-sm-12">
+                    <div className="card">
+                        <div className="row card-body">
+                            <div className="col-sm-9">
+                                <h5 className="card-title">{this.props.title}</h5>
+                                <p className="card-text">{this.props.description}</p>
+                                <a href="https://github.com/fatihdumanli/EventTower" target="_blank" className="btn btn-primary">Explore on GitHub</a>
+                            </div>
+                            <img className="col-sm-3" src={this.props.thumbnail} alt="sans" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default ProjectItem
